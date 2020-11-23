9ee6fc2b1e61015586e39dba5cba184d62b84830
Nadyan <nadyan_sp@hotmail.com>
2020-11-23 20:45:18 -0300

    class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import ListaDeNotas from './components/ListaDeNotas';
+import { Component } from 'react';
+import Formulario from './components/Formulario';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <Formulario />
+        <ListaDeNotas />
+      </div>
+    );
+  }
+  
 }
-
-export default App;
